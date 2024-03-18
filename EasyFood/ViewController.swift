10b3ab1bb3856dc43221804a1c7f00db4197196c
Huynh Phuc Nguyen Nguyen <hngu0112@student.monash.edu>
2024-03-18 14:18:48 +1100

    Initial Commit

---
+++ EasyFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyFood
+//
+//  Created by Nick Nguyen on 18/3/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
